aa6a1238388efaf991d1ffd40967b10bb8c65516
Mohammad Ali Sakib <mohammad.ali.sakib.pk@gmail.com>
2021-09-28 14:59:10 +0600

    initial file are added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Welcome to the react</h2>
+    </div>
+  );
+}
+
+export default App;
